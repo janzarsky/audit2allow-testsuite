bbe1f5cca80aaa002acdeb5f045e52775a54ffee
Jan Zarsky <jan.zarsky@gmail.com>
2018-01-15 15:29:09 +0100

    generate real AVC in portcon test

--- tests/portcon/runtest.sh
+++ tests/portcon/runtest.sh
@@ -2,37 +2,65 @@
 
 rlJournalStart
     rlPhaseStartSetup
-        # we expect that there is pulseaudio module present which contains
-        # following rules:
-        #
-        #   type pulseaudio_t;
-        #   type pulseaudio_port_t;
-        #   portcon tcp 4713 system_u:object_r:pulseaudio_port_t:s0
-        #   portcon udp 4713 system_u:object_r:pulseaudio_port_t:s0
-        #
+        rlAssertExists "test.c" || rlDie
+        rlAssertExists "test_module.te" || rlDie
 
-        rlRun "true >/var/log/audit/audit.log"
+        rlRun "checkmodule -m -M test_module.te -o test_module.mod"
+        rlRun "semodule_package -m test_module.mod -o test_module.pp"
+        rlRun "semodule -i test_module.pp"
+
+        rlRun "gcc test.c -o test -lselinux"
     rlPhaseEnd
 
     rlPhaseStartTest
-        # AVC: pulseaudio_t was denied name_bind on udp port 14 (which is
-        # reserved_port_t)
-        # TODO: generate real AVC
-        avc='type=AVC msg=audit(1458086688.481:1296): avc: denied { name_bind } for src=14 scontext=system_u:system_r:pulseaudio_t:s0 tcontext=system_u:object_r:reserved_port_t:s0 tclass=udp_socket';
+        rlRun "true >/var/log/audit/audit.log"
 
-        rlRun "echo '$avc' | audit2allow"
+        # run test program with context 'my_test_domain_t' and try to bind
+        # tcp port 14
+        rlRun "runcon -t my_test_domain_t ./test 14 > output" 1
+        rlRun "cat output"
 
-        # audit2allow should not provide allow rule, pulseaudio_t would gain
+        # check context of the process
+        rlRun "grep 'getcon: unconfined_u:unconfined_r:my_test_domain_t:s0-s0:c0.c1023' output"
+        # check that the process failed on bind()
+        rlRun "grep 'bind(...) = -1' output"
+
+        # there should be AVC message that name_bind was denied
+        rlRun "ausearch -m avc | grep 'denied  { name_bind }'"
+
+        # audit2allow should not provide allow rule, my_test_domain_t would gain
         # access to all reserved ports
-        rlRun "echo '$avc' | audit2allow | grep 'allow pulseaudio_t reserved_port_t:udp_socket name_bind;'" 1
+        rlRun "ausearch -m avc | audit2allow | grep 'allow my_test_domain_t reserved_port_t:tcp_socket name_bind;'" 1
 
         # audit2allow should provide semanage port command
         # 
-        #   semanage port -a -t pulseaudio_port_t -p udp 14
+        #   semanage port -a -t my_test_domain_t -p tcp 14
         #
-        rlRun "echo '$avc' | audit2allow | grep 'semanage port'"
+        rlRun "ausearch -m avc | audit2allow | grep 'semanage port'"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "true >/var/log/audit/audit.log"
+
+        # label the port using semanage port command
+        rlRun "semanage port -a -t my_test_domain_port_t -p tcp 14"
+
+        # run test program, it should now succeed
+        rlRun "runcon -t my_test_domain_t ./test 14 > output" 0
+        rlRun "cat output"
+
+        # check context of the process
+        rlRun "grep 'getcon: unconfined_u:unconfined_r:my_test_domain_t:s0-s0:c0.c1023' output"
+        # check that the process did not fail on bind()
+        rlRun "grep 'bind(...) = 0' output"
+
+        # there should not be any AVC messages
+        rlRun "ausearch -m avc" 1
     rlPhaseEnd
 
     rlPhaseStartCleanup
+        rlRun "semanage port -d -p tcp 14"
+        rlRun "semodule -r test_module"
+        rlRun "rm output test test_module.pp test_module.mod"
     rlPhaseEnd
 rlJournalEnd
